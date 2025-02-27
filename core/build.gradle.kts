a36e63ef85406537812634cc6c03eac5b0aca715
Gasper Lukman <gaspercino.lukman@gmail.com>
2025-02-27 21:01:59 +0100

    Libs fix

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:$minecraftVersion-R0.1-SNAPSHOT")
 
     compileOnly(libs.votingPlugin)
+    compileOnly(libs.hikariCP)
 
     compileOnly(libs.coinsEngine){
         exclude(group = "org.spigotmc", module = "spigot-api")
